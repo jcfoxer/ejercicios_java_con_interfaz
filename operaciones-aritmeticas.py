f4e4965f9ff4ccfde4d08e0a12606307a5e288c9
JUAN CARLOS COTRINA ROMAN <43387467+jcfoxer@users.noreply.github.com>
2023-06-14 18:25:12 -0500

    Add files via upload

---
+++ operaciones-aritmeticas.py
@@ -0,0 +1,5 @@
+num1=int(input("escribir el numero 1:"))
+num2=int(input("escribir el numero 2:"))
+resultado=num1+num2
+print("el resultadoi es :")
+print(resultado)
---
+++ prueba2.py
@@ -0,0 +1,31 @@
+"""mensaje=" bienmbermnvidos"
+mensaje+=" a"
+mensaje+=" python"
+print(mensaje)"""
+
+
+
+"""mensaje1=" bienmbermnvidos"
+mensaje2=" a"
+mensaje3=" python"
+print(mensaje1,mensaje2,mensaje3)"""
+
+"""mensaje1=" bienmbermnvidos"
+mensaje2=" a"
+mensaje3=" python"
+print(mensaje1+mensaje2+mensaje3)"""
+
+"""esta son las tres formas de mostrar texto"""
+
+"""---------------------------------------------------------"""
+"""porgrama que busca una subcadena en una cadena de texto"""
+
+"""mensaje="bienvenidos al curso de python desde skill"
+subcadena=mensaje.find("skill")
+print(subcadena)"""
+
+
+
+mensaje="bienvenidos al curso de python desde skill"
+subcadena=mensaje[14:16]
+print(subcadena)
